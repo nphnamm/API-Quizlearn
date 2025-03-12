0a7c49b7f91c33d9728a7b6ad287606d4761ade0
NamNPH <nphnamm@gmail.com>
2025-03-12 17:28:29 +0700

    update api

--- controllers/userProgresses.controller.ts
+++ controllers/userProgresses.controller.ts
@@ -2,79 +2,84 @@ import { Request, Response, NextFunction } from "express";
 import { CatchAsyncError } from "../middleware/CatchAsyncError";
 import ErrorHandler from "../utils/errorHandler";
 import db from "../models/index";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { Op } from "sequelize";
 const UserSession = db.UserSession;
 const UserProgress = db.UserProgress;
 const Card = db.Card;
 
 interface CustomRequest extends Request {
-    user?: any;
+  user?: any;
 }
 // Create Folder
 export const UpdateProgress = CatchAsyncError(
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const { sessionId, cardId, isCorrect } = req.body;
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { sessionId, cardId, isCorrect } = req.body;
 
+      const existingProgress = await UserProgress.findOne({
+        where: {
+          sessionId: sessionId,
+          cardId: cardId,
+        },
+      });
+      console.log("is", existingProgress);
+      let result;
+      if (!existingProgress) {
+        // Update existing record
+        result = await UserProgress.create({
+          id: uuidv4(),
+          sessionId,
+          cardId,
+          isCorrect,
+        });
+      } else {
+        result = await existingProgress.update({
+          isCorrect: isCorrect,
+        });
+      }
 
-            const existingProgress = await UserProgress.findOne({
-                where: {
-                    sessionId: sessionId,
-                    cardId: cardId
-                }
-            });
-            console.log('is',existingProgress)
-            let result;
-            if (!existingProgress) {
-                // Update existing record
-                result = await UserProgress.create({
-                    id: uuidv4(),
-                    sessionId,
-                    cardId,
-                    isCorrect
-                });
-            }else{
-                result = await existingProgress.update({
-                    isCorrect:isCorrect
-                })
-            }
-
-            res.status(200).json({
-                success: true,
-                result
-            });
-
-        } catch (error: any) {
-            return next(new ErrorHandler(error.message, 500));
-        }
+      res.status(200).json({
+        success: true,
+        result,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
     }
+  }
 );
 
 export const RestartSession = CatchAsyncError(
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const { sessionId } = req.body;
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { sessionId } = req.body;
 
+      const session = await UserSession.findByPk(sessionId);
+      //   console.log("session", session);
+      session.completed = false;
+      await session.save();
 
-            const existingProgress = await UserProgress.delete({
-                where: {
-                    sessionId: sessionId,
-                }
-            });
-            console.log('is',existingProgress)
-            
-            const updateSession = await UserSession.update({
-                isCompleted:false
-            })
-            res.status(200).json({
-                success: true,
-                message:"restart successfully!",
-                updateSession
-            });
+      const existingProgress = await UserProgress.findAll({
+        where: {
+          sessionId: sessionId,
+        },
+      });
+      await Promise.all(
+        existingProgress.map(async (progress: any) => {
+          progress.isCorrect = false;
+          await progress.save();
+        })
+      );
+      console.log(existingProgress);
+      console.log("is", existingProgress);
 
-        } catch (error: any) {
-            return next(new ErrorHandler(error.message, 500));
-        }
+      res.status(200).json({
+        success: true,
+        message: "restart successfully!",
+        session,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
     }
+  }
 );
