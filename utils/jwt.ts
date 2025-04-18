8137e10336bd479d39174ba2cb3dc964643cf92c
NamNPH <nphnamm@gmail.com>
2025-04-18 11:54:03 +0700

    update for deploy

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -21,14 +21,14 @@ export const accessTokenOptions: ITokenOptions = {
     
     expires: new Date(Date.now() + accessTokenExpire * 3 * 60 * 60 * 1000),
     maxAge: refreshTokenExpire * 3 * 60 * 60 * 1000,
-    httpOnly: true,
+    httpOnly: false,
     sameSite: 'none',
 }
 
 export const refreshTokenOptions: ITokenOptions = {
     expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
     maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
-    httpOnly: true,
+    httpOnly: false,
     sameSite: 'none',
 }
 
@@ -46,8 +46,9 @@ export const sendToken = (user: User, statusCode: number, res: Response) => {
     //only set secure to true in production
     if (process.env.NODE_ENV === 'production') {
         accessTokenOptions.secure = true;
+        refreshTokenOptions.secure = true; // ⚠️ Thêm dòng này
     }
-
+    
     res.cookie("access_token", accessToken, accessTokenOptions);
     res.cookie("refresh_token", refreshToken, refreshTokenOptions);
 
