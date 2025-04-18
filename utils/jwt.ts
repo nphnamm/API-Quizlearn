c4e6a7ac6a7b764a669c34922241982f56ac3635
NamNPH <nphnamm@gmail.com>
2025-04-18 11:44:19 +0700

    update backend for deploy

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -22,14 +22,14 @@ export const accessTokenOptions: ITokenOptions = {
     expires: new Date(Date.now() + accessTokenExpire * 3 * 60 * 60 * 1000),
     maxAge: refreshTokenExpire * 3 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: 'none',
 }
 
 export const refreshTokenOptions: ITokenOptions = {
     expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
     maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: 'none',
 }
 
 export const sendToken = (user: User, statusCode: number, res: Response) => {
