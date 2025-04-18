0f6169514b9016d851f974b5c6189ba0a553663c
NamNPH <nphnamm@gmail.com>
2025-04-18 12:23:39 +0700

    update

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -21,14 +21,14 @@ export const accessTokenOptions: ITokenOptions = {
     
     expires: new Date(Date.now() + accessTokenExpire * 3 * 60 * 60 * 1000),
     maxAge: refreshTokenExpire * 3 * 60 * 60 * 1000,
-    httpOnly: false,
+    httpOnly: true,
     sameSite: 'none',
 }
 
 export const refreshTokenOptions: ITokenOptions = {
     expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
     maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
-    httpOnly: false,
+    httpOnly: true,
     sameSite: 'none',
 }
 
