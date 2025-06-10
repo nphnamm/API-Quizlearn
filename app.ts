4db26793a3e8a58324da9de885d3e50bb888a33c
nphnamm <nphnamm@gmail.com>
2025-06-10 19:57:57 +0700

    update port

--- app.ts
+++ app.ts
@@ -110,7 +110,7 @@ const startServer = async () => {
   */
 
   app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
   });
 };
 
